a6f81e0ba72d35e0a6f220c2eeb14b16ba69c735
ijwebber <isaacjwebber@gmail.com>
2019-10-23 14:24:08 +0100

    added to database.py

--- database.py
+++ database.py
@@ -6,6 +6,28 @@ class Database:
     def __init__(self):
         pass
 
-    def getPlayer(self):
+    def getPlayer(self, username):
         #Will look up a player and return the player object from info from the database
-        return player.Player("Isaac", "capo")
+        return player.Player("Isaac", "capo", username)
+    
+    def newPlayer(self, player):
+        pass
+        #with sqlite3.connect("db.db")
+
+    def checkPlayerExists(self, username):
+        with sqlite3.connect("db.db") as db:
+            cursor = db.cursor()
+            Variable = username
+            cursor.execute("select username from Players where Username=?", (Variable,))
+            select = cursor.fetchall()
+        if select:
+            print("User exists")
+        else:
+            print("User does not exist")
+        
+
+def main():
+    data = Database()
+    data.checkPlayerExists("ijwebber")
+
+main()
