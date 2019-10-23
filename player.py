df56e1c066a7ffc4bd473bc886e523c808e333ff
ijwebber <isaacjwebber@gmail.com>
2019-10-23 10:26:53 +0100

    added the username attribute to player and also getters and setters

--- player.py
+++ player.py
@@ -1,14 +1,23 @@
 class Player:
 
-    def __init__(self, name, keyword):
+    def __init__(self, name, keyword, username):
         self.__name = name
         self.__keyword = keyword
+        self.__username = username
 
+    #Getters
     def getName(self):
         return self.__name
     
-    def updateName(self, newName):
+    def getUsername(self):
+        return self.__username
+
+    #Setters
+    def setName(self, newName):
         self.__name = newName
+    
+    def setUsername(self, newUsername):
+        self.__username = newUsername
     
     def checkKey(self, newKeyword):
         return (self.__keyword == newKeyword)
