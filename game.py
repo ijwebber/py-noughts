36253823f30a221f0475f2a45f6b8e7474af5542
ijwebber <isaacjwebber@gmail.com>
2019-10-26 22:47:06 +0100

    win check works for horizontal wins

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ class Game:
     def getOtherPlayer(self):
         return self.__players[abs(self.__currentPlayer - 1)]
 
-    def isEmpty(self, x, y):
+    def isEmptySlot(self, x, y):
         if self.__board[y][x] == " ":
             return True
         else:
@@ -32,7 +32,27 @@ class Game:
     def __incrementCurrentPlayer(self):
         self.__currentPlayer = abs(self.__currentPlayer - 1)
 
+    def __checkForWinner(self):
+        b = self.__board
+        #set winner to whoever the winner is
+        #Vertical
+
+        #Horizontal
+        i = 0
+        for i in range(0,3):
+            if b[i][0] == b[i][1] and b[i][1] == b[i][2] and b[i][0] != " ":
+                self.__winner = b[0][0]
+                return True
+        return False
+
+    def __checkForDraw(self):
+        return False
+    
     def isNotOver(self):
+        if self.__checkForWinner():
+            return False
+        if self.__checkForDraw():
+            return False
         return True
     
     def takeTurn(self):
@@ -45,7 +65,7 @@ class Game:
                 y = int(input("y: "))
                 if y > 2 or y < 0:
                     raise Exception
-                if self.isEmpty(x,y):
+                if self.isEmptySlot(x,y):
                     self.updateBoard(x,y)
                     notValid = False
                 else:
